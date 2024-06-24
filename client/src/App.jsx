01f6d3284ffebf7c0f983b11242daefd66dfc8cd
ignacio <ignaciocarrabs@gmail.com>
2024-06-24 19:01:39 -0300

    menu and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,77 @@
 import React from "react";
+import "./styles.scss"
+
 import Navbar from "./components/navbar/Navbar.jsx";
+import Footer from "./components/footer/Footer.jsx";
+import Changas from "./pages/changas/Changas.jsx"
+import Changa from "./pages/changa/Changa.jsx"
+import Message from "./pages/message/Message.jsx"
+import Messages from "./pages/messages/Messages.jsx"
+import Orders from "./pages/orders/Orders.jsx"
+import Add from "./pages/add/Add.jsx"
+import MyChangas from "./pages/myChangas/MyChangas.jsx"
+
+
+import { createBrowserRouter, RouterProvider, Outlet} from "react-router-dom";
+import Home from "./pages/home/Home.jsx"
 
 function App() {
+
+  const Layout = () => {
+    return (
+      <>
+      <Navbar/>
+      <Outlet/>
+      <Footer/>
+      </>
+    )
+  }
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout/>,
+      children: [
+        {
+          path:"/",
+          element:<Home/>
+        },
+        {
+          path:"/changas",
+          element:<Changas/>
+        },
+        {
+          path:"/changa/:id",
+          element:<Changa/>
+        },
+        {
+          path:"/orders",
+          element:<Orders/>
+        },
+        {
+          path:"/mischangas",
+          element:<MyChangas/>
+        },
+        {
+          path:"/messages",
+          element:<Messages/>
+        },
+        {
+          path:"/message/:id",
+          element:<Message/>
+        },
+        {
+          path:"/add",
+          element:<Add/>
+        }
+      ]
+    },
+  ]);
+
+
   return (
     <>
-      <Navbar />
-      
+      <RouterProvider router={router}/>
     </>
   );
 }
--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,47 +1,81 @@
 import React, { useEffect, useState } from "react";
 import "./Navbar.scss";
+import { Link, useLocation } from "react-router-dom";
 
 const Navbar = () => {
+  const [active, setActive] = useState(false);
+  const [open, setOpen] = useState(false)
 
-    const [active, setActive] = useState(false)
-    
-    const isActive = () =>{
-        window.scrollY > 0 ? setActive(true) : setActive(false)
-    }
 
-    useEffect(() => {
-      window.addEventListener("scroll", isActive)
-    
-      return () => {
-        window.removeEventListener("scroll", isActive)
-      }
-    }, [])
-    
+  const { pathname } = useLocation()
+
+  const isActive = () => {
+    window.scrollY > 0 ? setActive(true) : setActive(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", isActive);
+
+    return () => {
+      window.removeEventListener("scroll", isActive);
+    };
+  }, []);
+
+  const currentUser = {
+    id: 1,
+    isSeller: true,
+    username: "Jhon Dae",
+  };
 
   return (
-    <div className={active ? "navbar active" : "navbar"}>
+    <div className={active || pathname !=="/" ? "navbar active" : "navbar"}>
       <div className="container">
-        <div className="logo">
+        <div className="logo" >
+          <Link to="/" className="link">
+          <img src="/assets/images/763396.png" />
           <span className="text">Laburito</span>
+          </Link>
         </div>
         <div className="links">
           <span>Laburito Business</span>
           <span>Explorar</span>
           <span>Español</span>
-          <span>Iniciar Sesión</span>
-          <span>Ofrecer mis changas</span>
-          <button>Unirse</button>
+          {!currentUser && <span>Iniciar Sesión</span>}
+          {!currentUser?.isSeller && <span>Ofrecer mis changas</span>}
+          {!currentUser && <button>Unirse</button>}
+          {currentUser && (
+            <div className="user" onClick={()=>setOpen(!open)}>
+              <img src="https://w7.pngwing.com/pngs/315/234/png-transparent-graphy-young-blond-man-men-s-health-male-man-thumbnail.png" alt="" />
+              <span>{currentUser.username}</span>
+              {open && <div className="options">
+                {currentUser?.isSeller && (
+                  <>
+                    <Link className="link" to="/changas">Changas</Link>
+                    <Link className="link" to="/add">Agregar una nueva changa</Link>
+                    <Link className="link" to="/orders">Ordenes</Link>
+                  </>
+                )}
+              <Link className="link" to="/messages">Mensajes</Link>
+              <Link className="link">Cerrar sesión</Link>
+              </div>}
+            </div>
+          )}
         </div>
       </div>
-      {active && (<><hr />
-      <div className="menu">
-        <span>Test 1</span>
-        <span>Test 2</span>
-        <span>Test 3</span>
-        <span>Test 4</span>
-        <span>Test 5</span>
-      </div></>)
-      }
+      {(active || pathname !=="/") && (
+        <>
+          <hr />
+          <div className="menu">
+            <Link className="link" to="/">Albañileria</Link>
+            <Link className="link" to="/">Plomeria</Link>
+            <Link className="link" to="/">Gasista</Link>
+            <Link className="link" to="/">Jardineria</Link>
+            <Link className="link" to="/">Mantenimiento</Link>
+            <Link className="link" to="/">Reparaciones</Link>
+            <Link className="link" to="/">Mantenimiento</Link>
+          </div>
+        </>
+      )}
     </div>
   );
 };
